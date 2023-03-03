4bd3e3393207cec3179f8e55f3e5bb49be06994e
Ramaiah1971A <126606906+Ramaiah1971A@users.noreply.github.com>
2023-03-03 12:39:39 +0530

    updated

--- Realme.groovy
+++ Realme.groovy
@@ -5,7 +5,7 @@ pipeline {
         Env_Type = 'NP'
     }
     Parameter {
-        Choice (name: 'DC' Choices: ['HAresh', 'QA', 'Sprint'], description: 'Select QA Environment for deployment')
+        choice (name: 'DC' choices: ['HAresh', 'QA', 'Sprint'], description: 'Select QA Environment for deployment')
     }
     stages {
             stage('Build') {
