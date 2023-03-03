2b73cdb7d12bdbd2751482d8c952f543c49b4157
Ramaiah1971A <126606906+Ramaiah1971A@users.noreply.github.com>
2023-03-03 12:55:33 +0530

    update

--- Realme.groovy
+++ Realme.groovy
@@ -4,31 +4,24 @@ pipeline {
         Env_Name = 'QA'
         Env_Type = 'NP'
     }
-    Parameter {
+    parameters {
         choice(name: 'DC', choices: ['HAresh', 'QA', 'Sprint'], description: 'Select QA Environment for deployment')
     }
     stages {
-            stage('Build') {
-                steps {
-                    echo ' his name is hareesh'
-                }
+        stage('Build') {
+            steps {
+                echo ' his name is hareesh'
             }
-            stage('Test'){
-                steps {
-                    echo 'his name is kiran'
-                }
-            }
-            stage('Deploy') {
-                steps {
-                    echo 'his name is harsha'
-                }
-            }
-            stage('Demo Active Choices Parameter') {
-                steps {
-                    echo "States Selected ${states}"
-                    echo "Cities Selected ${cities}"
-                }
-            }
-            
         }
+        stage('Test'){
+            steps {
+                echo 'his name is kiran'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'his name is harsha'
+            }
+        }
+    }
 }
